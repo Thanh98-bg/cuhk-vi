1a9227a675789de072ffb8d51fa1cf5ebc4bfe1c
Thanh98-bg <thanhbg4117@gmail.com>
2021-05-19 20:47:08 +0700

    update processed token

---
+++ update_processed_token_vi.py
@@ -0,0 +1,20 @@
+import os
+import json
+import codecs
+from underthesea import chunk
+from underthesea import word_tokenize
+
+os.chdir('/home/thanhnguyen/Test/cuhk-vi')
+myfile = 'cuhk_vi_20202.json'
+cnt= 0
+with open(myfile, 'r', encoding = "utf-8") as f0:
+    json_data = json.load(f0)
+for id in range(len(json_data)):
+    if "captions" in json_data[id]:
+        for cap_id in range(len(json_data[id]['captions'])):
+            json_data[id]['captions'][cap_id] = word_tokenize(json_data[id]['captions'][cap_id], format="text")
+            json_data[id]['processed_tokens'][cap_id] = word_tokenize(json_data[id]['captions'][cap_id])
+    cnt = cnt + 1
+    print(str(cnt))
+with codecs.open('reid_raw.json', 'w', "utf-8") as outfile:
+    outfile.write(json.dumps(json_data, ensure_ascii=False))
