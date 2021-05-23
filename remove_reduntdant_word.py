de2659098a34c789e2310d21a8e2142485e6fe96
Thanh98-bg <thanhbg4117@gmail.com>
2021-05-23 16:31:14 +0700

    remove reduntdant tokens

---
+++ remove_reduntdant_word.py
@@ -0,0 +1,28 @@
+import os
+import json
+import codecs
+os.chdir('/home/thanhnguyen/ViTAA/datasets/cuhkpedes/text_attribute_graph')
+cnt = 0
+for file in os.listdir():
+    cnt = cnt + 1
+    with open(file,'r' ,encoding="utf-8") as f:
+        json_data = json.load(f)
+    for att in json_data:
+        for key in att:
+            arr = att[key]
+            my_dict = {}
+            for i in range(len(arr)):
+                if not arr[i] in my_dict:
+                    my_dict[arr[i]] = 1
+                else:
+                    my_dict[arr[i]] = my_dict[arr[i]] + 1
+            arr.clear()
+            for key in my_dict:
+                arr.append(key)
+            print(arr)
+        print(json_data)
+    print(str(cnt) + " " + file)
+    if cnt == 100 :
+        break
+    #with codecs.open(file, 'w', "utf-8") as outfile:
+        #outfile.write(json.dumps(json_data, ensure_ascii=False))
